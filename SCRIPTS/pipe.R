cd10951d489f972a632f2084497dbfb28528ea52
PaulRivaud <paulrivaud.info@gmail.com>
2022-08-22 14:37:22 +0200

    added clusters figure

--- SCRIPTS/pipe.R
+++ SCRIPTS/pipe.R
@@ -46,6 +46,13 @@ obj <- FindNeighbors(obj, reduction = "pca", dims = 1:30)
 obj <- FindClusters(obj, verbose = FALSE)
 obj <- RunUMAP(obj, reduction = "pca", dims = 1:30)
 
+save_pdf_path <- sprintf("%s.clusters.pdf",gsub(".rds$","",save_seurat_obj_path))
+pdf(save_pdf_path,height=15,width=20)
+p1 <- DimPlot(brain, reduction = "umap", label = TRUE)
+p2 <- SpatialDimPlot(brain, label = TRUE, label.size = 3)
+p1 + p2
+dev.off()
+
 # find spatially variable features
 obj <- FindSpatiallyVariableFeatures(obj, assay = "SCT", features = VariableFeatures(obj)[1:100],selection.method = "markvariogram")
 
